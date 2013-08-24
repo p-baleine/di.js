7139917b2d7de12ec8eada625af44cdc9e0d91bd
Tajima Junpei <p.baleine@gmail.com>
2013-08-24 09:45:35 +0900

    change export strategy

--- di-lite.js
+++ di-lite.js
@@ -11,7 +11,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-di = {
+var di = module.exports = {
     version: "0.3.3",
     createContext: function () {
         var ctx = {
